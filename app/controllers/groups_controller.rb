e8c187174967f51b67ed76172195bc046f773fa9
soto-ogre <soto1oog@gmail.com>
2019-11-13 13:32:58 +0900

    show group list in sidebar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
-    @groups = Group.all
+    
   end
 
   def new
@@ -19,9 +19,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -32,7 +29,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, {:user_ids => []})
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
   def set_group
