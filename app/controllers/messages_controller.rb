00e7710bb60018cace7f8808266cb62aface5d05
soto-ogre <soto1oog@gmail.com>
2019-11-19 15:59:27 +0900

    edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
+    if @message.save  
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
