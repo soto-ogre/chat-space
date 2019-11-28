a6ab9d5d13de6200c0a2fa07ba290d7b87b0b1c8
soto-ogre <soto1oog@gmail.com>
2019-11-28 11:17:32 +0900

    Implemented ajax
     Implemented ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+      var img = message.image.url ? `<img src= ${ message.image.url }>` : "";
+      var html = `<div class="content__post">
+                  <div class="content__data">
+                  <div class="content__user">
+                  ${message.name}
+                  </div>
+                  <div class="content__time">
+                  ${message.created_at}
+                  </div>
+                  </div>
+                  <p class="content__message">
+                  </p><p>
+                  ${message.content}
+                  </p>
+                  ${img}
+                  </div>`
+    return html;
+}
+
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.content__talk').append(html);
+      $('.content__talk').animate({ scrollTop: $('.content__talk')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('メッセージ送信に失敗しました');
+    })
+  })
+});
